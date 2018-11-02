38c47893f7005bd5b7d2ca131a30d8043ebdc6cf
myoung20 <37824193+myoung20@users.noreply.github.com>
2018-11-01 17:26:35 -0700

    Add files via upload

---
+++ GCI_Python.py
@@ -0,0 +1,15 @@
+#Print "GCI is great" 10 times
+i = 0
+while i < 10:
+    print("GCI is great")
+    i = i + 1
+
+#Ask the user for his/her name
+name = input("What is your name? ")
+
+#Print "Hello (name), please to meet you!"
+print ("Hello", name, "please to meet you!")
+
+#Print name backwards
+print (name[::-1])
+    
